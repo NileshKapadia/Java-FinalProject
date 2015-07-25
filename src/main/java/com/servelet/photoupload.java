960ffcee417913a0ec8b7ce98ab7a47e473e06c5
NIlesh <nileshkapadiya04@gmail.com>
2015-07-25 18:12:28 -0400

    commit

--- src/main/java/com/servelet/photoupload.java
+++ src/main/java/com/servelet/photoupload.java
@@ -15,6 +15,7 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,6 +28,7 @@ import javax.servlet.http.Part;
  * @author NILESH
  */
 @WebServlet(name = "photoupload", urlPatterns = {"/photoupload"})
+@MultipartConfig(maxFileSize = 16177215)
 public class photoupload extends HttpServlet {
     
     
