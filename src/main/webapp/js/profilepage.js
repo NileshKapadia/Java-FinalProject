13ca78663bbe17e4550aace8c62b846b7147701a
NIlesh <nileshkapadiya04@gmail.com>
2015-07-27 19:34:34 -0400

    commit

--- src/main/webapp/js/profilepage.js
+++ src/main/webapp/js/profilepage.js
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+$(document).ready(function(){
+    
 
 $("#reg").click(function (e) {
        e.preventDefault();
@@ -72,6 +74,7 @@ alert('dflhdxfibhfi');
 
 
 
+   });
    });
 
 
