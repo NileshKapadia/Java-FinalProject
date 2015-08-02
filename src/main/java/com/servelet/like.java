92713e4c1258513e5670ef0ea1a0358e54ce4783
NIlesh <nileshkapadiya04@gmail.com>
2015-08-02 19:10:27 -0400

    commit

---
+++ src/main/java/com/servelet/like.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.servelet;
+
+import databaseCredential.databaseconnection;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author NILESH
+ */
+@WebServlet(name = "like", urlPatterns = {"/like"})
+public class like extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet like</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet like at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        PrintWriter pt = response.getWriter();
+
+       
+        String image_id = request.getParameter("image_id");
+        
+        
+
+       
+        Connection conn = databaseconnection.getConnection();
+        Statement smt;
+        try {
+            smt = conn.createStatement();
+
+            String sql = "INSERT INTO like(img_id , like) "
+                    + "VALUES ('" + image_id + "' , like)";
+
+            smt.executeUpdate(sql);
+
+            response.sendRedirect("jsp/login.jsp");
+
+        } catch (SQLException ex) {
+            Logger.getLogger(sigup.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+    }
+
+}
